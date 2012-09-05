44f4e59045399da2defe38005a3b05075aafb5b4
raoz <raotallinn.ee@gmail.com>
2012-09-05 19:36:21 +0300

    SETUP

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ludum24page::Application.config.secret_token = 'ec5190ce210066bfad37f2d06f9dc787ceb67f56cb97b569c7ca4982ce1d59ce9456fde1835f6295644e78a690cb9edf3b310802b5bd99dabee558e406088fa9'
