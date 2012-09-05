75986ec6dc9e7630b000852f521989a3a1e5cb9b
raoz <raotallinn.ee@gmail.com>
2012-09-05 21:25:02 +0300

    Refactored

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ludum24page::Application.config.secret_token = 'ec5190ce210066bfad37f2d06f9dc787ceb67f56cb97b569c7ca4982ce1d59ce9456fde1835f6295644e78a690cb9edf3b310802b5bd99dabee558e406088fa9'
+Ludum24page::Application.config.secret_token = 'e442dc830f63130b7c5dc220b3f0586a913606958fe1a67a692250a63daf020414fe740e180951d1b96b95aca0995b60cb95eb3deb880fc966bf4ce1e72ea301'
